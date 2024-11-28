35f16c915196e526300a27705cefedd727bb4be0
KLA Gueu <klagueu@gmail.com>
2024-11-27 16:25:28 -0800

    removing middleware

--- src/pages/api/middleware.ts
+++
@@ -1,6 +0,0 @@
-// src/pages/api/_middleware.ts
-
-import { bearerTokenMiddleware } from '@/middleware/trueRequest';
-
-// Appliquer le middleware à toutes les routes de l'API
-export { bearerTokenMiddleware as default} from '@/middleware/trueRequest';
